9eada6e0fd04cb28abbd3a8a6368efc158d26373
CooolRyan <74342019+CooolRyan@users.noreply.github.com>
2025-05-22 12:10:09 +0900

    🎉init : 어드민 웹 init

---
+++ wooreadmin/src/main/java/com/piehouse/wooreadmin/WooreadminApplication.java
@@ -0,0 +1,13 @@
+package com.piehouse.wooreadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooreadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooreadminApplication.class, args);
+    }
+
+}
